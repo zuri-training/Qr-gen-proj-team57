96243fc1d8d4fbb5c29e052b7fac5620956fc09f
Lilywd <ndungulilianwanjiku@gmail.com>
2022-08-15 16:07:53 +0300

    initial-commit

--- Backend/qrcodegen/settings.py
+++ Backend/qrcodegen/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY =env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = [".herokuapp.com”]
 
 
 # Application definition
